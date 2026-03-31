be1276270dbe9ad5891b4af6c728cf80d51ee567
payablijah <elijah@payabli.com>
2026-03-31 17:06:05 -0400

    revamped react and vue apps

--- react-integration/astro.config.mjs
+++ react-integration/astro.config.mjs
@@ -1,9 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  server: {
+    host: true,
+    allowedHosts: true
+  },
+  security: {
+    checkOrigin: false
+  },
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
