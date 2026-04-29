d5d63bb6fbb05e3d6d40107840e8652de21e9fb8
payablijah <elijah@payabli.com>
2026-04-28 21:15:18 -0400

    fixed npm upgrade and npm update

--- temp-token/astro.config.mjs
+++ temp-token/astro.config.mjs
@@ -1,16 +1,20 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
-import tailwind from "@astrojs/tailwind";
-
+import node from "@astrojs/node";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
-
 import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
+  vite: {
+    plugins: [tailwindcss()],
+  },
   integrations: [
-    tailwind(),
     react(),
     alpinejs(),
   ],
